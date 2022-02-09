7e03b3b0f483f3489c5e0b5f316f6ba093056e50
Louis Delmas <louis.delmas@outlook.com>
2022-02-09 15:17:11 +0100

    d

--- article_sentences_classification.py
+++ article_sentences_classification.py
@@ -1,77 +1,43 @@
+import numpy as np
 import pandas as pd
+from sklearn import metrics
 import spacy
-import torch
-from transformers import CamembertForSequenceClassification, CamembertTokenizer
-
 from extract_annotated_sentences import get_sentences_from_doc
-from tagtog import get_text_from_id
-# from extract_annotated_sentences import get_topic_sentences
 
-MODEL_PATH = "articles_sentences.model"
+from sentences_classification import load_model_tokenizer, predict
 
-def preprocess(raw_articles, tokenizer, labels=None):
-    """
-        Create pytorch dataloader from raw data
-    """
-
-    # https://huggingface.co/docs/transformers/internal/tokenization_utils#transformers.PreTrainedTokenizerBase.batch_encode_plus.truncation
-
-    encoded_batch = tokenizer.batch_encode_plus(raw_articles,
-                                                add_special_tokens=False,
-                                                padding = True,
-                                                truncation = True,
-                                                max_length = 128,
-                                                return_attention_mask=True,
-                                                return_tensors = 'pt')
-        
-
-    if labels:
-        labels = torch.tensor(labels)
-        return encoded_batch['input_ids'], encoded_batch['attention_mask'], labels
-    return encoded_batch['input_ids'], encoded_batch['attention_mask']
-
-def predict(articles, model, tokenizer):
-    with torch.no_grad():
-        model.eval()
-        input_ids, attention_mask = preprocess(articles, tokenizer)
-        output = model(input_ids, attention_mask=attention_mask)
-        print(output[0])
-        return torch.argmax(output[0], dim=1)
+def classify_from_sentences_labels(predicted_labels):
+    (unique, counts) = np.unique(predicted_labels, return_counts=True)
+    count_labels = np.asarray((unique, counts)).T
+    max_idx = np.argmax(count_labels[:, 1])
+    return count_labels[max_idx][0]
 
 if __name__ == "__main__":
-    ##
-    article_ids = [10202, 3000, 2999, 10204]
-    ##
-
-    model = CamembertForSequenceClassification.from_pretrained(
-        'camembert-base',
-        num_labels = 3
-    )
-    model.load_state_dict(torch.load(MODEL_PATH))
-    model.eval()
-
-    tokenizer = CamembertTokenizer.from_pretrained(
-        'camembert-base',
-        do_lower_case=True
-    )
+    model, tokenizer = load_model_tokenizer()
 
     nlp = spacy.load("fr_core_news_md", exclude=["parser"])
     nlp.enable_pipe("senter")
 
-    data = []
+    df = pd.read_csv("datasets/articles/test_text_dataset.csv", usecols=["label", "article"])
+    n_rows = len(df)
+    labels = df.label.values - 1
+    contents = df.article.values
 
-    for article_id in article_ids:
-        content = get_text_from_id(article_id)
+    predicted_labels = []
 
-        doc = nlp(content)
+    for id, content in enumerate(contents):
+        print(f'{id}/{n_rows}')
+        doc = nlp(str(content))
         sentences = get_sentences_from_doc(doc)
+        predicted_sentences_labels = predict(sentences, model, tokenizer)
+        predicted_label = classify_from_sentences_labels(predicted_sentences_labels)
+        predicted_labels.append(predicted_label)
 
-        predicted_labels = predict(sentences, model, tokenizer).tolist()
+    confusion_matrix = metrics.classification_report(labels, predicted_labels, zero_division=0)
+    report = metrics.confusion_matrix(labels, predicted_labels)
 
-        for sentence, label in zip(sentences, predicted_labels):
-            data.append((article_id, label, sentence))
-            print(f'({article_id}) {label}: {sentence}')
-
-    df = pd.DataFrame(data, columns=["article_id", "label", "text"])
-    df.to_csv("article_sentences_classification.csv")
+    print("PREDICT ON TEST SET")
+    print(report)
+    print(confusion_matrix)
+        
         
